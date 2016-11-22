082494b027e3ce308a5d389b07ece8a7c6e436f3
Marc Mims <marc@questright.com>
2016-11-22 09:36:38 -0800

    Optimize NormalizeBooleans
    No use running through all the possible boolean args. There will be far
    fewer args. Also reduces it to a single loop. And true-only booleans
    don't need to be declared twice.

--- lib/Twitter/API/Trait/NormalizeBooleans.pm
+++ lib/Twitter/API/Trait/NormalizeBooleans.pm
@@ -17,32 +17,34 @@ around preprocess_args => sub {
 # Twitter usually accepts 1, 't', 'true', or false for booleans, but they
 # prefer 'true' or 'false'. In some cases, like include_email, they only accept
 # 'true'. So, we normalize these options.
-sub boolean_args {
-    qw/
-        contributor_details display_coordinates exclude_replies hide_media
-        hide_thread hide_tweet include_email include_entities include_my_tweet
-        include_rts include_user_entities map omit_script possibly_sensitive
-        reverse skip_status skip_user trim_user
-    /;
-}
+my @normal_bools = qw/
+    contributor_details display_coordinates exclude_replies hide_media
+    hide_thread hide_tweet include_email include_entities include_my_tweet
+    include_rts include_user_entities map omit_script possibly_sensitive
+    reverse trim_user
+/;
 
 # Workaround Twitter bug: any value passed for these options are treated as
 # true.  The only way to get 'false' is to not pass the skip_user at all. So,
 # we strip these boolean args if their values are false.
-sub true_only_booleans { qw/skip_status skip_user/ }
+my @true_only_bools = qw/skip_status skip_user/;
+
+my %is_bool = map { $_ => undef } @normal_bools, @true_only_bools;
+my %is_true_only_bool = map { $_ => undef } @true_only_bools;
+
+sub is_bool { exists $is_bool{$_[1]} }
+
+sub is_true_only_bool { exists $is_true_only_bool{$_[1]} }
 
 sub normalize_bools {
     my ( $self, $args ) = @_;
 
     # Twitter prefers 'true' or 'false' (requires it in some cases).
-    for my $k ( $self->boolean_args ) {
-        next unless exists $args->{$k};
-        next if $args->{$k} =~ /^true|false$/;
+    for my $k ( keys %$args ) {
+        next unless $self->is_bool($k);
         $args->{$k} = $args->{$k} ? 'true' : 'false';
-    }
-
-    for my $k ( $self->true_only_booleans ) {
-        delete $args->{$k} if exists $args->{$k} && $args->{$k} eq 'false';
+        delete $args->{$k} if $self->is_true_only_bool($k)
+            && $args->{$k} eq 'false';
     }
 }
 
---
+++ t/trait-normalize-booleans.t
@@ -0,0 +1,33 @@
+#!perl
+use 5.12.1;
+use warnings;
+
+use Test::More;
+
+package Foo {
+    use Moo;
+    with 'Twitter::API::Trait::NormalizeBooleans';
+
+    # required
+    sub preprocess_args {}
+}
+
+my $args = {
+    foo           => 'bar',
+    include_email => 1,
+    skip_user     => 't',
+    skip_status   => '',
+    text          => 'test',
+};
+
+my $foo = Foo->new;
+$foo->normalize_bools($args);
+
+is_deeply $args, {
+    foo           => 'bar',
+    include_email => 'true',
+    skip_user     => 'true',
+    text          => 'test',
+}, 'normalized';
+
+done_testing;
